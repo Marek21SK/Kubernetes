7e906ff108a388d008f499d123ef81f150583e2e
Marek Pástor <marekpastorml@gmail.com>
2023-04-13 16:28:24 +0200

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var path = require('path');
-//var mysql = require('mysql');
+var mysql = require('mysql2');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'secretKey',
+  resave: false,
+  saveUninitialized: true
+}));
 /*
 // Vytvorenie pripojenia k databáze
 const connection = mysql.createConnection({
@@ -28,24 +36,53 @@ process.on('exit', function(){
   console.log('Spojenie s MySQL databázou ukončené.');
   connection.end();
 });
-/*
-connection.end((err) => {
-  if (err) throw err;
-  console.log('Spojenie s MySQL databázou ukončené.');
-});
 */
-
 // Definícia endpointov
-app.get('/', function (req, res) {
+app.get('/index.html', function (req, res) {
   console.log('Main index');
-  res.sendFile("./index.html", {root: __dirname});
+  var name = req.session.name || '';
+  res.sendFile("./index.html", {root: __dirname},{name: name});
 });
 
 app.get('/kubernetes', function (req, res) {
-  console.log('Index Kubernetes');
+  console.log('Kubernetes');
   res.sendFile("./kubernetes.html", {root: __dirname});
 });
+
+app.get('/login', function (req, res) {
+  console.log('Login');
+  res.sendFile("./login.html", {root: __dirname});
+});
 /*
+app.post('/login', function (req, res){
+  var name = req.body.name;
+  var password = req.body.password;
+
+  connection.query('SELECT * FROM users WHERE name = ? AND password = ?', [name, password], (err, rows) => {
+    if (err) throw err;
+    if (rows.length > 0) {
+      req.session.name = name; // uložíme meno používateľa do session
+      console.log('Prihlásený!')
+      res.sendFile(path.join(__dirname, '/public/index.html'));
+    } else {
+      console.log('Nepsrávne prihlasovacie údaje!')
+      res.sendFile(path.join(__dirname, '/public/login.html'));
+    }
+  });
+});
+
+/*
+// Odhlásenie používateľa
+app.get('/logout', function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.sendFile(path.join(__dirname, '/public/index.html'));
+    }
+  });
+});
+*//*
 app.get('/users', function (req, res) {
   connection.query('SELECT * FROM users', (err, rows) => {
     if (err) throw err;
