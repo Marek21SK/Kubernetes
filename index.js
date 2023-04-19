85741746456f026621388f734daf5f0856e76188
Marek Pástor <marekpastorml@gmail.com>
2023-04-19 10:41:23 +0200

    commit

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+
+// Vytvorenie pripojenia k databáze
+const connection = mysql.createConnection({
+  host: 'mysql-service',
+  port: 3306,
+  user: 'root',
+  password: 'password',
+  database: 'kubernetes'
+});
+
 /*
+//Cez toto pripojenie k databáze som si testoval serverovu časť mojej node js aplikácie.
 // Vytvorenie pripojenia k databáze
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -22,6 +33,7 @@ const connection = mysql.createConnection({
   password: 'password',
   database: 'kubernetes'
 });
+*/
 
 // Test pripojenia k databáze
 connection.connect((err) => {
@@ -36,7 +48,7 @@ process.on('exit', function(){
   console.log('Spojenie s MySQL databázou ukončené.');
   connection.end();
 });
-*/
+
 // Definícia endpointov
 app.get('/index.html', function (req, res) {
   console.log('Main index');
@@ -53,7 +65,7 @@ app.get('/login', function (req, res) {
   console.log('Login');
   res.sendFile("./login.html", {root: __dirname});
 });
-/*
+
 app.post('/login', function (req, res){
   var name = req.body.name;
   var password = req.body.password;
@@ -62,7 +74,7 @@ app.post('/login', function (req, res){
     if (err) throw err;
     if (rows.length > 0) {
       req.session.name = name; // uložíme meno používateľa do session
-      console.log('Prihlásený!')
+      console.log('Prihlásený! ' + req.session.name);
       res.sendFile(path.join(__dirname, '/public/index.html'));
     } else {
       console.log('Nepsrávne prihlasovacie údaje!')
@@ -71,18 +83,55 @@ app.post('/login', function (req, res){
   });
 });
 
-/*
-// Odhlásenie používateľa
-app.get('/logout', function (req, res) {
-  req.session.destroy(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get('/form', function (req, res){
+  //Ak používateľ nie je prihlásený, presmerujeme ho na LOGIN
+  if(!req.session.name){
+    return res.sendFile(path.join(__dirname, '/public/login.html'));
+  }
+  res.sendFile(path.join(__dirname, '/public/kubernetes.html'));
+});
+
+app.post('/form', function (req, res){
+  //Ak používateľ nie je prihlásený, presmerujeme ho na LOGIN
+  if (!req.session.name){
+    return res.sendFile(path.join(__dirname, '/public/login.html'));
+  }
+
+  //Získanie id používateľa z tabuľky users
+  var query1 = "SELECT id FROM users WHERE name =?";
+  var values1 = [req.session.name];
+
+  connection.query(query1, values1, function(error, results, fields){
+    if(error){
+      console.log(error);
+      res.send('Chyba pri získavaní ID používateľa');
+    }else{
+      var userid = results[0].id; //uloženie id používateľa do premennej
+      console.log('Výpis: ID používateľa '+ userid);
+    
+      var meno = req.body.meno;
+      var priezvisko = req.body.priezvisko;
+      var email = req.body.email;
+      var mesto = req.body.mesto;
+
+      //Vytvorenie dotazu na pridanie dát do tabuľky
+      var query = "INSERT INTO forms (users_id, username, surename, email, city) VALUES (?, ?, ?, ?, ?)";
+      var values = [userid, meno, priezvisko, email, mesto];
+
+      //Spustenie dotazu na databázu
+      connection.query(query, values, function(error, results, fields){
+        if (error){
+          console.log(error);
+          res.send('Chyba pri vkladaní dát do databázy');
+        }else{
+          console.log('Dáta boli úspešne vložené do databázy používateľom: ' + req.session.name);
+          res.sendFile(path.join(__dirname, '/public/index.html'));
+        }
+      });
     }
   });
 });
-*//*
+
 app.get('/users', function (req, res) {
   connection.query('SELECT * FROM users', (err, rows) => {
     if (err) throw err;
@@ -91,7 +140,7 @@ app.get('/users', function (req, res) {
     res.json(rows);
   });
 });
-*/
+
 // Spustenie servera na porte 3000
 app.listen(3000, () => {
   console.log('Server spustený na porte 3000...');
